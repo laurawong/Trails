d16febb2c80123b56ffe00c66778997f0247022e
lauraw <laura.wong21@gmail.com>
2020-04-30 17:00:06 -0700

    Fix wave icon

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,13 @@ import '../styles/trails-patterns.scss';
 
 class About extends Component {
   render() {
+    const waveEmoji = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/237/waving-hand-sign_emoji-modifier-fitzpatrick-type-1-2_1f44b-1f3fb_1f3fb.png'
+    
     return (
       <div id='about' class='about-container'>
         <div class='about-text text'>
           <h1>
-            Hello <span role='img' aria-label="wave">👋🏻</span>
+            Hello <span role='img' aria-label="wave"><img class='wave-icon' src={waveEmoji} alt="wave emoji" /></span>
           </h1>
           <p>
             My name is Laura. This is my side project powered by React and my love for hiking.
